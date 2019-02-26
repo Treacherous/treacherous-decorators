48f0a6a4ae34373f08bb6d2d704482c47aee1e0e
Lee Prosser <lee.prosser@embersoft.co.uk>
2019-02-26 07:35:00 +0000

    Bumped version and added umd output

--- build/tasks/compile.js
+++ build/tasks/compile.js
@@ -1,7 +1,9 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var merge = require('merge2');
-var paths = require("../paths");
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const webpack = require('webpack-stream');
+const runSequence = require("run-sequence");
+const merge = require('merge2');
+const paths = require("../paths");
 
 var compileFor = function(moduleType, withTypings = false, target = "es2015") {
     var tsProject = ts.createProject('tsconfig.json', {
@@ -22,9 +24,26 @@ var compileFor = function(moduleType, withTypings = false, target = "es2015") {
     return tsResult.js.pipe(gulp.dest(paths.dist + "/" +moduleType));
 }
 
-gulp.task('compile', ["clean", "generate-exports"], function() {
+
+gulp.task("compile-webpack", function() {
+    return gulp.src(`${paths.dist}/es2015/index.js`)
+        .pipe(webpack({
+            output: {
+                filename: "treacherous.decorators.umd.js",
+                library: "TreacherousDecorators",
+                libraryTarget: "umd"
+            }
+        }))
+        .pipe(gulp.dest(`${paths.dist}/umd`))
+});
+
+gulp.task('compile-modules', function(){
     return merge([
         compileFor("commonjs", true, "es5"),
         compileFor("es2015")
-    ]);
+    ])
+});
+
+gulp.task('compile', ["clean", "generate-exports"], function(callback) {
+    return runSequence('compile-modules', 'compile-webpack', callback);
 });
--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -4,12 +4,12 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./exposer"));
+__export(require("./helpers/metadata-helper"));
 __export(require("./decorators/with-display-name"));
 __export(require("./decorators/with-rule-foreach"));
 __export(require("./decorators/with-rule"));
 __export(require("./decorators/with-ruleset-foreach"));
 __export(require("./decorators/with-ruleset"));
-__export(require("./helpers/metadata-helper"));
 __export(require("./decorators/shortcuts/date"));
 __export(require("./decorators/shortcuts/decimal"));
 __export(require("./decorators/shortcuts/email"));
